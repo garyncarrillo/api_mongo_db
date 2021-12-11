c9c4cb6e26f0b18ee85cc32fb869eb2d583b1ca7
Garyn Carrillo <iwdev@Garyns-MacBook-Pro.local>
2021-12-10 20:36:53 -0500

    Add router, schema  and .env

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 9000;
+const userRoutes = require("./routes/users");
+app.use(express.json());
+app.use("/api",userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my first API");
 });
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Conectado a la base de datos Cluster Atlas'))
+.catch((error) => console.console.log(error));
 
 app.listen(port, () => {
   console.log('Server running on port '+port);
---
+++ src/routes/users.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const userSchema = require("../models/user");
+const router = express.Router();
+
+//crear user
+router.post("/users", (req, res) => {
+  const user = userSchema(req.body);
+
+  user
+  .save()
+  .then((data) => res.json(data))
+  .catch((error) =>  res.json({ message: error }))
+});
+
+//listar todos
+router.get("/users", (req, res) => {
+  userSchema
+  .find()
+  .then((data) => res.json(data))
+  .catch((error) => res.json({ message: error }))
+});
+
+// obtener un usuario
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  userSchema
+  .findById(id)
+  .then((data) => res.json(data))
+  .catch((error) => res.json({ message: error }))
+});
+
+// actualizar un usuario
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, age, email } = req.body;
+
+  userSchema
+  .updateOne( {_id: id},  { $set: { name, age, email }})
+  .then((data) => res.json(data))
+  .catch((error) => res.json({ message: error }))
+});
+
+
+//eliminar un usuario
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+  .remove({ _id: id })
+  .then((data) => res.json(data))
+  .catch((error) =>  res.json({ message: error }))
+});
+
+module.exports = router;
